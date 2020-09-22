41b90dbfa56efb5df13b8809241e9bb5aa4fe559
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-09-21 22:55:48 -0500

    pizza api routes created and working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api/index')
 
 router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
 
 module.exports = router;
+
+
