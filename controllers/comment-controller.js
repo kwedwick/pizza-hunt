ba3bc88bba6ad7d4b429c37491fd2dbcd1b01d85
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-09-24 15:56:05 -0500

    replies done

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,6 +21,22 @@ const commentController =  {
         })
         .catch(err => res.json(err));
     },
+
+    addReply({ params, body }, res) {
+        Comment.findByIdAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+    },
     // remove comment
     removeComment({ params }, res) {
         Comment.findByIdAndDelete({ _id: params.commentId })
@@ -41,6 +57,15 @@ const commentController =  {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+    removeReply({ params }, res) {
+        Comment.findByIdAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
