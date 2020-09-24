c1c5760c2d60bff0f0c75abb69c6baf081ffd8f7
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-09-24 18:30:29 -0500

    validators added

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,10 +5,14 @@ const { Schema, model } = require('mongoose');
 const PizzaSchema = new Schema(
     {
         pizzaName: {
-            type: String
+            type: String,
+            required: 'You need to provide a pizza name!',
+            trim: true
         },
         createdBy: {
-            type: String
+            type: String,
+            required: 'You need to provide who created it!',
+            trim: true
         },
         createdAt: {
             type: Date,
@@ -17,8 +21,10 @@ const PizzaSchema = new Schema(
         },
         size: {
             type: String,
+            required: true,
+            enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
             default: 'Large'
-        },
+          },
         toppings: [],
         comments: [
             {
